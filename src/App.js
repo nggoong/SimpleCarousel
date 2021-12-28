5caccc65d7b7e84607c28b20bf26c93995a9c5eb
nggoong <nggoong@naver.com>
2021-12-29 00:24:36 +0900

    carousel slider next방향으로 무한 슬라이드 구현 완료✔

--- src/App.js
+++ src/App.js
@@ -3,25 +3,39 @@ import Boxcomponent from './Boxcomponent';
 import React, {useRef, useEffect, useState} from 'react';
 
 function App() {
-  const numbers = ['1','2','3','4'];
+  const numbers = ['1','2','3','4','1'];
   // const [index, setIndex] = useState(1);
-  const index = useRef(0);
+  let index = 0
 
   const sliderRef = useRef();
 
 
   const prevHandler = () => {
-    if(index.current === 0) return;
-    index.current -= 1;
-    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index.current)}px, 0, 0)`;
+    if(index === 0) return;
+    index -= 1;
+    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
     
     
   }
+  const tempFunc = () => {
+    sliderRef.current.style.transition = `0ms`;
+    index = 0;
+    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
+    console.log('complete')
+
+  }
 
   const nextHandler = () => {
-    if(index.current === 3) return;
-    index.current += 1;
-    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index.current)}px, 0, 0)`;
+    if(index=== 3) {
+      sliderRef.current.addEventListener('transitionend', tempFunc);
+    }
+    if(index !== 3) {
+      sliderRef.current.style.transition = `0.2s`;
+      sliderRef.current.removeEventListener('transitionend', tempFunc);
+    }
+    index += 1;
+    
+    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
     
 
   }
