3eb441a3778df347fed571daa08b255d1aa9b8b0
nggoong <nggoong@naver.com>
2021-12-31 19:57:14 +0900

    양방향 무한 carousel slider 기능 구현 완료✔

--- src/App.js
+++ src/App.js
@@ -3,32 +3,56 @@ import Boxcomponent from './Boxcomponent';
 import React, {useRef, useEffect, useState} from 'react';
 
 function App() {
-  const numbers = ['1','2','3','4','1'];
+  const numbers = ['4','1','2','3','4','1'];
   // const [index, setIndex] = useState(1);
-  let index = 0
+  let index = 1
+
+  let mouseClick = false;
 
   const sliderRef = useRef();
 
 
   const prevHandler = () => {
-    if(index === 0) return;
+    mouseClick = false;
+    sliderRef.current.removeEventListener('transitionend', MoveFunc);
+    if(index === 1)  {
+      sliderRef.current.addEventListener('transitionend', MoveFunc);
+      console.log('end')
+    }
+    else if(index !== 1) {
+      sliderRef.current.style.transition = `0.1s`;
+    }
     index -= 1;
     sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
     
-    
   }
-  const tempFunc = () => {
-    sliderRef.current.style.transition = `0ms`;
-    index = 0;
-    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
-    console.log('complete')
 
+  // const nextMoveFunc = () => {
+  //   sliderRef.current.style.transition = `0ms`;
+  //   index = 0;
+  //   sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
+  //   console.log('complete')
+
+  // }
+
+  const MoveFunc = (e) => {
+    sliderRef.current.style.transition='0ms';
+    index = !mouseClick? 4:0;
+    console.log(e.target.className);
+    sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
   }
+
+  // const prevMoveFunc = () => {
+  //   sliderRef.current.style.transition='0ms';
+  //   index = 4;
+  //   sliderRef.current.style.transform = `translate3d(${-500 * parseInt(index)}px, 0, 0)`;
+  // }
 
   const nextHandler = () => {
-    sliderRef.current.removeEventListener('transitionend', tempFunc);
+    mouseClick = true;
+    sliderRef.current.removeEventListener('transitionend', MoveFunc);
     if(index=== 3) {
-      sliderRef.current.addEventListener('transitionend', tempFunc);
+      sliderRef.current.addEventListener('transitionend', MoveFunc);
     }
     else if(index !== 3) {
       sliderRef.current.style.transition = `0.1s`;
@@ -42,8 +66,8 @@ function App() {
 
   return (
    <div className='section'>
-     <button onClick={prevHandler}> prev </button>
-     <button onClick={nextHandler}> next </button>
+     <button className = 'prev-btn' onClick={prevHandler}> prev </button>
+     <button className = 'next-btn' onClick={nextHandler}> next </button>
      <div className='carousel-container'>
        <div className='carousel-viewer'>
          <div className='carousel-slider' ref={sliderRef}>
